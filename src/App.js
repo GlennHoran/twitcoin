c73b47af08b19fd515b5ca384d24c60249be1071
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2018-06-08 17:07:53 +0100

    playing with flexbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './twitcoin.gif';
+import logo from './images/twitcoin.gif';
+import twitter from './images/TwitterSample.png'
+import chart from './images/SampleChart.png'
 import './App.css';
 
 class App extends Component {
@@ -7,10 +9,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <p className="App-intro">
-
-            </p>
+            <div className="introText">
+                <div className="block">
+                    <img src={logo} alt="logo" align="center" className = "logo"/>
+                </div>
+                <div className="block">
+                    <p className = "leftText">Neural net aided Twitter sentiment analysis for the prediction of bitcoin prices</p>
+                </div>
+            </div>
+            <div className="container">
+                <div className="block">
+                    <img src={twitter} alt="twitter" className="image"/>
+                </div>
+                <div className = "block">
+                    <img src={chart} alt="chart" className="image"/>
+                </div>
+            </div>
         </header>
       </div>
     );
